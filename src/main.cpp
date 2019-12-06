f732a59b20be1495e514c596d421af5e7fbd7c7f
unknown <felipemello@gmail.com>
2019-12-05 23:48:25 -0300

    Added route draws

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,78 @@
 #include <iomanip>
-#include "TabuSearchSVRP.h"
 #include <algorithm>
 #include <ctime>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include <io.h>
+#include "TabuSearchSVRP.h"
 
 char verbosity;
 
-int main() {
+int main(int argc, const char **argv) {
 
   Graph graph;
   double fillingCoeff;
 	int capacity, numberVertices, numberVehicles;
+  char saveFile;
+  ifstream instanceFile;
+  stringstream input;
+  string line;
 
-  do {
-    cout << "Enter with number of vertices including depot (>1): ";
-    cin >> numberVertices;
-  } while(numberVertices <= 1);
+  if(argc == 2) {
+    instanceFile.open(argv[1], std::ios::in | std::ios::binary);
 
-	do{
-    cout << "Enter with number of vehicles (>=1 and less then number of vertices): ";
-    cin >> numberVehicles;
-  } while(numberVehicles < 1 || numberVehicles > numberVertices-1);
+  	if (!instanceFile.is_open()) {
+    		printf("ERROR: Not possible to open instance file.\n");
+  		return 1;
+  	}
 
-  do {
-    cout << "Enter with filling coefficient in interval (0,1]: ";
-    cin >> fillingCoeff;
-  } while(fillingCoeff <= 0 || fillingCoeff > 1);
+    getline(instanceFile, line);
+		input = stringstream(line);
+		input >> numberVertices;
+    getline(instanceFile, line);
+		input = stringstream(line);
+		input >> numberVehicles;
+    getline(instanceFile, line);
+    input = stringstream(line);
+    input >> fillingCoeff;
+    getline(instanceFile, line);
+    input = stringstream(line);
+    input >> verbosity;
+    getline(instanceFile, line);
+    input = stringstream(line);
+    input >> saveFile;
+  }
 
-  do {
-    cout << "Visualize all problem information? (y/n): ";
-    cin >> verbosity;
-  } while(verbosity != 'y' && verbosity != 'n');
+  else {
+
+    do {
+      cout << "Enter with number of vertices including depot (>1): ";
+      cin >> numberVertices;
+    } while(numberVertices <= 1);
+
+    do{
+      cout << "Enter with number of vehicles (>=1 and less then number of vertices): ";
+      cin >> numberVehicles;
+    } while(numberVehicles < 1 || numberVehicles > numberVertices-1);
+
+    do {
+      cout << "Enter with filling coefficient in interval (0,1]: ";
+      cin >> fillingCoeff;
+    } while(fillingCoeff <= 0 || fillingCoeff > 1);
+
+    do {
+      cout << "Visualize all problem information in stdio? (y/n): ";
+      cin >> verbosity;
+    } while(verbosity != 'y' && verbosity != 'n');
+
+    do {
+      cout << "Save best solution in txt file? (y/n): ";
+      cin >> saveFile;
+    } while(saveFile != 'y' && saveFile != 'n');
+
+  }
 
 	/* Capacidade regulada de acordo com os dados do problema */
 	capacity = max(int(10*(numberVertices-1)/(2*numberVehicles*fillingCoeff)),20);
@@ -43,8 +86,6 @@ int main() {
   if(verbosity == 'y')
   	graph.printInstance();
 
-  graph.drawGraph("instancia_entrada");
-
 	TabuSearchSVRP ts;
 
   clock_t begin = clock();
@@ -55,16 +96,91 @@ int main() {
 
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-  cout << "Melhor solucao viavel encontrada:" << endl;
-	for(int i = 0; i < bestSol.routes.size(); i++) {
-		cout << "Rota " << i << ": ";
-		for(int j = 0; j < bestSol.routes[i].size(); j++) {
-			cout << bestSol.routes[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << "Custo total: " << bestSol.expectedCost << endl;
-  cout << "Tempo de processamento: " << elapsed_secs << endl;
+  if(saveFile == 'y') {
+    ofstream outputFile;
+    string nameOutputFile = "output\\";
+
+    nameOutputFile += "BestSolN" + to_string(numberVertices)
+                    + "M" + to_string(numberVehicles)
+                    + "f" + to_string(fillingCoeff).substr(0,4) + ".txt";
+
+    mkdir("output");
+
+    outputFile.open(nameOutputFile, std::ios::app);
+
+    if (outputFile.is_open())  {
+
+      if(bestSol.routes.size() == 0)
+          outputFile << "Nenhuma solucao viavel encontrada" <<  endl;
+
+      else {
+
+        for(int i = 0; i < bestSol.routes.size(); i++) {
+      		outputFile << "Rota " << i << ": ";
+      		for(int j = 0; j < bestSol.routes[i].size(); j++) {
+      			outputFile << bestSol.routes[i][j] << " ";
+      		}
+      		outputFile << endl;
+      	}
+      }
+
+      outputFile << "Custo total: " << bestSol.expectedCost << endl;
+      outputFile << "Tempo de processamento: " << elapsed_secs << endl << endl;
+
+      outputFile.close();
+
+      vector<int> l(graph->numberVertices, 0);
+      vector<vector<int>> routeMatrix(graph->numberVertices, v);
+
+      for(int i = 0; i < bestSol.routes.size(); i++) {
+
+        if(bestSol.routes[i].size() > 0) {
+
+          routeMatrix[0][bestSol.routes[i][0]] = 1;
+
+          int j;
+          for(j = 0; j < bestSol.routes[i].size()-1; j++) {
+            routeMatrix[bestSol.routes[i][j]][bestSol.routes[i][j+1]] = 1;
+          }
+
+          routeMatrix[bestSol.routes[i][j]][0] = 1;
+        }
+      }
+
+      for(int i = 0; i < graph->numberVertices; i++) {
+
+        for(int j = 0; j < graph->numberVertices; j++) {
+
+          if(routeMatrix[i][j] != 1)
+            graph.adjMatrix[i][j] = 0;
+        }
+      }
+
+      graph.drawGraph(outputFile);
+    }
+
+    else cout << "Unable to open file";
+  }
+
+  else {
+
+    if(bestSol.routes.size() == 0)
+        cout << "Nenhuma solucao viavel encontrada" <<  endl;
+
+    else {
+      for(int i = 0; i < bestSol.routes.size(); i++) {
+        cout << "Rota " << i << ": ";
+        for(int j = 0; j < bestSol.routes[i].size(); j++) {
+          cout << bestSol.routes[i][j] << " ";
+        }
+        cout << endl;
+      }
+    }
+
+    cout << "Custo total: " << bestSol.expectedCost << endl;
+    cout << "Tempo de processamento: " << elapsed_secs << endl << endl;
+
+  }
 
   return 0;
 
